35669c0f0270d39f1bc0dbc63c6de18b586e2b3e
SimonS2019 <shentiexiong@gmail.com>
2024-08-29 10:44:44 +1200

    use function to fetch data 1

---
+++ learningapp/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace learningapp;
+
+public class Course
+{
+    public int CourseID { get; set; }
+    public string? CourseName { get; set; }
+    public decimal Rating { get; set; }
+
+    public Course()
+    {
+        
+    }
+
+}
